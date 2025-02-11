4cd15ab6888555d35590c971ebf4635ec8ea0025
MaksimStep <maksimstep9889@gmail.com>
2025-02-11 14:32:59 +0300

    Задание 2

--- homework.py
+++ homework.py
@@ -1,4 +1,3 @@
-
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -8,21 +7,30 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if 0 <= grade <= 10:
+                if course in lecturer.grades:
+                    lecturer.grades[course] += [grade]
+                else:
+                    lecturer.grades[course] = [grade]
+            else:
+                return 'Ошибка'
+        else:
+            return 'Ошибка: Лектор не ведет этот курс у этого студента'
+
 class Mentor:
     def __init__(self, name, surname, courses_attached):
         self.name = name
         self.surname = surname
         self.courses_attached = courses_attached
-
 class Lecturer(Mentor):
     def __init__(self, name, surname, courses_attached):
         super().__init__(name, surname, courses_attached)
         self.grades = {}
-
 class Reviewer(Mentor):
     def __init__(self, name, surname, courses_attached):
         super().__init__(name, surname, courses_attached)
-
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -31,14 +39,3 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_reviewer = Reviewer('Some', 'Buddy', ['Python']) 
-
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
