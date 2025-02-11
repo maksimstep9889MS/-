95077e2436b8172955272b238b4d8394dd0803d5
MaksimStep <maksimstep9889@gmail.com>
2025-02-11 14:28:21 +0300

    Задание 1

---
+++ homework.py
@@ -0,0 +1,44 @@
+
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+class Mentor:
+    def __init__(self, name, surname, courses_attached):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = courses_attached
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname, courses_attached):
+        super().__init__(name, surname, courses_attached)
+        self.grades = {}
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname, courses_attached):
+        super().__init__(name, surname, courses_attached)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_reviewer = Reviewer('Some', 'Buddy', ['Python']) 
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
